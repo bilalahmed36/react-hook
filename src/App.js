d611b2c563d0a08178b32ced18aabbf59fde2455
bilalahmed36 <71526359+bilalahmed36@users.noreply.github.com>
2020-10-31 16:14:07 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Candy from "./candy"
+import './App.css';
+
+function App() {
+  let [count, setCount] = useState(1);
+  let [isMorning, setMorning] = useState(true);
+  return (
+    <div className={`App-header ${isMorning ? 'App' : ''}`}>
+      <h3>Day Status = {isMorning ? 'Morning' : 'Night'}</h3>
+      <Candy counter = {count}/>
+      <button onClick = {() => setCount(++count)}>Add More</button>
+      <br/>
+      <button onClick={()=> setMorning(!isMorning)}>Dark Mode</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/candy.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Candy(props) {
+    return (
+        <div>
+            <h1>Hello From {props.counter}</h1>
+        </div>
+    );
+}
+
+export default Candy;
